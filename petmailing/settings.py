abdcd4c679361bd5aae4ca8c3925357b8fd0047d
Sultan <kadyevsultan521@gmail.com>
2024-11-14 15:40:12 +0600

    Old version for backup

--- petmailing/settings.py
+++ petmailing/settings.py
@@ -137,37 +137,18 @@ def email_verified_callback(user):
     user.is_active = True
 
 
-# def password_change_callback(user, password):
-#     user.set_password(password)
-
-
-# Global Package Settings
 EMAIL_FROM_ADDRESS = 'verify@gmail.com'  # mandatory
 EMAIL_PAGE_DOMAIN = 'http://127.0.0.1:8000/'  # mandatory (unless you use a custom link)
 EMAIL_MULTI_USER = False  # optional (defaults to False)
 
-# Email Verification Settings (mandatory for email sending)
 EMAIL_MAIL_SUBJECT = 'Подтверди свой email {{ user.username }}'
 EMAIL_MAIL_HTML = 'accounts/email/mail-body.html'
 EMAIL_MAIL_PLAIN = 'accounts/email/mail-body.txt'
 EMAIL_MAIL_TOKEN_LIFE = 60 * 60  # one hour
 
-# Email Verification Settings (mandatory for builtin view)
 EMAIL_MAIL_PAGE_TEMPLATE = 'accounts/email/email-success-template.html'
 EMAIL_MAIL_CALLBACK = email_verified_callback
 
-# # Password Recovery Settings (mandatory for email sending)
-# EMAIL_PASSWORD_SUBJECT = 'Смени свой пароль {{ user.username }}'
-# EMAIL_PASSWORD_HTML = 'accounts/password/password-body.html'
-# EMAIL_PASSWORD_PLAIN = 'accounts/password/password-body.txt'
-# EMAIL_PASSWORD_TOKEN_LIFE = 60 * 10  # 10 minutes
-
-# # Password Recovery Settings (mandatory for builtin view)
-# EMAIL_PASSWORD_PAGE_TEMPLATE = 'accounts/password/password-changed-template.html'
-# EMAIL_PASSWORD_CHANGE_PAGE_TEMPLATE = 'accounts/password/password-change-template.html'
-# EMAIL_PASSWORD_CALLBACK = password_change_callback
-
-# For Django Email Backend
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
